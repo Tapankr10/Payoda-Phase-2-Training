d93cbd282bcf716ff00c942c128a7e0ab5946485
Tapankr10 <tapan.k@payoda.com>
2024-08-26 22:07:10 +0530

    Add files via upload

---
+++ Daily Task/Day-6(26.08.2024)/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+SqlConnection conn =new SqlConnection(DataMisalignedException sourse =);
+
+conn.Open();
+SqlCommand cmd = new SqlCommand("create table Product ")
+    cmd
