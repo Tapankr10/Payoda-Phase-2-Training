60b5b34f2ea604dc1b7c8316e0ad3d8760f03b03
Tapankr10 <tapan.k@payoda.com>
2024-09-02 20:07:06 +0530

    Delete Daily Task/Day-11(02.09.2024)/Commerce/Commerce/Models/ErrorViewModel.cs

--- Daily Task/Day-11(02.09.2024)/Commerce/Commerce/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Commerce.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
