de43b119d86a1b0c1d778ac906bf62df56bbf030
Tapankr10 <tapan.k@payoda.com>
2024-09-04 23:01:55 +0530

    Add files via upload

---
+++ Daily Task/Day-13(04.09.2024)/Apiproject/Apiproject/Controllers/BookAuthorsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Apiproject.Model;
+
+namespace Apiproject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookAuthorsController : ControllerBase
+    {
+        private readonly BookDbConnext _context;
+
+        public BookAuthorsController(BookDbConnext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BookAuthors
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BookAuthor>>> GetBookAuthor()
+        {
+            return await _context.BookAuthor.Include(o => o.book ).Include(o => o.Author).ToListAsync();
+        }
+
+        // GET: api/BookAuthors/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BookAuthor>> GetBookAuthor(int id)
+        {
+            var bookAuthor = await _context.BookAuthor.FindAsync(id);
+
+            if (bookAuthor == null)
+            {
+                return NotFound();
+            }
+
+            return bookAuthor;
+        }
+
+        // PUT: api/BookAuthors/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBookAuthor(int id, BookAuthor bookAuthor)
+        {
+            if (id != bookAuthor.AuthorId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bookAuthor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookAuthorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BookAuthors
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<BookAuthor>> PostBookAuthor(BookAuthor bookAuthor)
+        {
+            _context.BookAuthor.Add(bookAuthor);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (BookAuthorExists(bookAuthor.AuthorId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetBookAuthor", new { id = bookAuthor.AuthorId }, bookAuthor);
+        }
+
+        // DELETE: api/BookAuthors/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBookAuthor(int id)
+        {
+            var bookAuthor = await _context.BookAuthor.FindAsync(id);
+            if (bookAuthor == null)
+            {
+                return NotFound();
+            }
+
+            _context.BookAuthor.Remove(bookAuthor);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BookAuthorExists(int id)
+        {
+            return _context.BookAuthor.Any(e => e.AuthorId == id);
+        }
+    }
+}
---
+++ Daily Task/Day-13(04.09.2024)/Apiproject/Apiproject/Model/BookAuthor.cs
@@ -0,0 +1,10 @@
+﻿namespace Apiproject.Model
+{
+    public class BookAuthor
+    {
+        public int BookId { get; set; }
+        public int AuthorId { get; set; }
+        public Book? book { get; set; }
+        public Author ? Author { get; set; }
+    }
+}
